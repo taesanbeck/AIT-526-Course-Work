77260de889d78bd332c856b088e6f13ce91242c9
Datageek2525 <63169589+taesanbeck@users.noreply.github.com>
2023-05-22 21:47:43 -0400

    c

--- Module_2/main_streamlit_eliza.py
+++ Module_2/main_streamlit_eliza.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from eliza_streamlit import reflect, analyze, talk_to_me
 
-"""THIS IS THE FRONT END FILE FOR THE STREAMLIT APP, NOT TO BE RUN ON ITS OWN"""
+# THIS IS THE FRONT END FILE FOR THE STREAMLIT APP, NOT TO BE RUN ON ITS OWN
 
 # Sources
 # https://docs.streamlit.io/
@@ -17,4 +17,6 @@ if statement:
     response = talk_to_me(statement)
     st.write('Eliza: ', response)
 
+st.markdown("[Link to GitHub Repository](https://github.com/taesanbeck/AIT-526-Course-Work/tree/main/Module_2)")
 
+st.markdown("### Contributors: Sean Lam, Henry Wu, George Cross, and Sid Beck")
